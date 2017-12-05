3a3cdbeee666547b663b89306beb90f37d1ec25d
KPHarmon <kade_harmon@yahoo.com>
2017-12-04 21:50:03 -0800

    Optimization
    Added wordlists, fixed overflow bug, revised game checking system, updated aesthetics, revised comments

--- Hangman.c
+++ Hangman.c
@@ -4,7 +4,10 @@
 #include <windows.h>
 #include <conio.h>
 #include <string.h>
+#include <assert.h>
+#define TRASH_LENGTH 1024
 
+//Initialize functions
 int main();
 int game();
 int playerOne();
@@ -12,19 +15,39 @@ int playerTwo();
 int options();
 int help();
 int quit();
+int win();
+int lose();
+
+//Word Lists functions
 int countries();
 int movies();
 int books();
+int brands();
+int people();
+int winter();
+int animals();
+int hackathon();
+
+//Initialize default amount of lives
 int counterLives = 8;
 char difficulty[7] = "MEDIUM";
 int persisLives;
 FILE *file;
-    
+char wordOne[30], wordTwo[30];
+
+//Main Menu
 int main(void)
 {
     system("cls");
     int mainInput;
-    printf("\nMAIN MENU:\n\n1:  PLAY\n2:  OPTIONS\n3:  HELP\n4:  QUIT\n");
+    printf("MAIN MENU:\n\n1:  PLAY\n2:  OPTIONS\n3:  HELP\n4:  QUIT\n");
+    printf("____________   \n");
+    printf("|         |    \n");
+    printf("|         0    \n");
+    printf("|        /|\\  \n");
+    printf("|        / \\  \n");
+    printf("|              \n");
+    printf("|              \n");
     scanf("%d", &mainInput);
     switch(mainInput)
     {
@@ -46,12 +69,12 @@ int main(void)
     }
 }
 
-//mAIN MENU FOR THE GAME
+//Single player Selection
 int game(void)
 {
     int countPlayer;
     system("cls");
-    printf("\n1:  SINGLE PLAYER\n2:  TWO+ PLAYERS\n3:  MAIN MENU\n");
+    printf("CHOOSE PLAYER SETTING:\n\n1:  SINGLE PLAYER\n2:  TWO+ PLAYERS\n3:  MAIN MENU\n");
     scanf("%d", &countPlayer);
     switch(countPlayer)
     {
@@ -70,30 +93,56 @@ int game(void)
     }
 }
 
+//Single player game
 int playerOne(void)
 {
+    srand((int)time(NULL));
     system("cls");
     int wordSize, i, j=0, input, counter = 0, catInput, calibrate = 0;
-    char wordOne[30], blanks[30], guess, wrongChars[9];
-    //COLLECTING STRING FROM TXT FILE
-    srand((int)time(NULL));
+    char blanks[30], guess, wrongChars[9];
     int count = 0;
-    printf("CHOOSE A CATEGORY:\n");
-    printf("1.  COUNTRIES\n");
-    printf("2.  MOVIES\n");
-    printf("3.  BOOKS\n");
+    persisLives = counterLives;     //persisLives value will change throughout run, will grab the static
+                                    //counterLives value before each run(selected in options or by default)
+    //Category Selection
+    printf("CHOOSE A CATEGORY:\n\n");
+    printf("1.  ANIMALS\n");
+    printf("2.  BOOKS\n");
+    printf("3.  BRANDS\n");
+    printf("4.  COUNTRIES\n");
+    printf("5.  HACKATHON\n");
+    printf("6.  MOVIES\n");
+    printf("7.  PEOPLE\n");
+    printf("8.  WINTER\n");
+    printf("--------------------\n");
     printf("9.  PLAYER SELECTION\n");
     scanf("%d", &catInput);
+    
+    //Workaround for C not liking multiple file pulls in same function
     switch(catInput)
     {
         case 1:
-            countries();
+            animals();
             break;
         case 2:
-            movies();
+            books();
             break;
         case 3:
-            books();
+            brands();
+            break;
+        case 4:
+            countries();
+            break;
+        case 5:
+            hackathon();
+            break;
+        case 6:
+            movies();
+            break;
+        case 7:
+            people();
+            break;
+        case 8:
+            winter();
             break;
         case 9:
             game();
@@ -102,76 +151,132 @@ int playerOne(void)
             main();
             break;
     }
+    
+    //Scan through scanning length of file, chooses a random line, pulls string, stores in variable wordOne
     char line[40];
     while(fgets(line, sizeof line, file) != NULL)
     {
+        char *p = strchr(line, '\n');
+        if (p != NULL) {
+            assert(*p == '\n');
+        *p = '\0';
+    }
+    else 
+    {
+        char trash[TRASH_LENGTH];
+        while((p = fgets(trash, TRASH_LENGTH, file)) != NULL) 
+        {
+            if ((p = strchr(trash, '\n')) != NULL)
+                break;
+        }
+    }
+    assert(strchr(line, '\n') == NULL);
+        
         count++;
         if ((rand() / (float)RAND_MAX) <= (1.0 / count)) 
         {
-            // reads text until newline
             fscanf(file, "%[^\n]", wordOne);
         }
     } 
     fclose(file);
     
+    //Store length of the found string
     wordSize = strlen(wordOne);
-    persisLives = counterLives;
-    //INITIALIZING ARRAY
+
+    //Initialize array blanks with underscores or slashes if it finds a space
     for(i = 0; i < wordSize; i++)
     {
         blanks[i] = '_';
         if(wordOne[i] == ' ')
         {
             blanks[i] = '/';
-            calibrate++;
+            calibrate++;      
         }
-    } 
+    }
+    
+    //Initialize array of incorrect guesses
     for(i = 0; i < 9; i++)
     {
         wrongChars[i] = '\0';
     }
     
-    //MAIN INTERFACE FOR PLAYER1 GAME
+    //While loop will continuously display updated loops until a person runs out of lives
     while(persisLives > 0)
     {
-        int flag = 0;
+        int flag = 0, flag2 = 0;
         system("cls");
-        //lives updater
+        
+        //Dislay number of lives
         printf(" LIVES: %d", persisLives);
         printf("\n\n");
-        //blanks updater
+        
+        //Display array of blanks
         for(i = 0; i < wordSize; i++)
         {
             printf(" %c", blanks[i]);
         }
-        //guesses tracker
+        
+        //Display array of wrong guesses
         printf("\n\nWRONG GUESSES:\n");
         for(i = 0; i < 9; i++)
         {
             printf(" %c", wrongChars[i]);
         }
         
-        //input letter, check to see if input character matches word, if not, take away life
+        //Scans for user input and checks to see if input matches character in generated word
         printf("\n\nGUESS A LETTER\n");
         scanf(" %c", &guess);
         for(i=0; i<wordSize; i++)
         {
-            if(wordOne[i] == guess)
-            {
+            if(wordOne[i] == guess  && blanks[i] == '_')
+             {
                 printf("\a");
                 blanks[i] = guess;
                 counter++;
                 flag = 1;
-            }
+             }
         }
+        
+        //If the input is not found, remove a life and add to array of wrong guesses
         if(flag == 0)
         {
             persisLives--;
             wrongChars[j] = guess;
-            j++; 
+            j++;
         }
-        if(persisLives == 0)
+        
+        //Checks to find underscores, if there is then change flag
+        for(i = 0; i<wordSize; i++)
         {
+            if(blanks[i] == '_')
+            {
+                flag2 = 1;
+                break;  
+            }
+        }
+        
+        //If the lives reaches zero and flag from above is triggered, display lose screen
+        if(flag2 == 1 && persisLives == 0)
+        {
+            system("cls");
+            
+            //Display final Lives
+            printf(" LIVES: %d\n", persisLives);
+            printf("\n");
+        
+            //Display final blanks
+            for(i = 0; i < wordSize; i++)
+            {
+                printf(" %c", blanks[i]);
+            }
+            
+            //Display final guesses
+            printf("\n\nWRONG GUESSES:\n");
+            for(i = 0; i < 9; i++)
+            {
+                printf(" %c", wrongChars[i]);
+            }
+            
             printf("\n\n\nYOU LOSE, THE CORRECT ANSWER IS %s\n\n\nTYPE '1' TO PLAY AGAIN\nTYPE '2' TO RETURN TO MAIN MENU\n", wordOne);
             scanf(" %d", &input);
             switch(input)
@@ -184,26 +289,35 @@ int playerOne(void)
                     break;
                 default:
                     printf("PICK '1' OR '2'");
+                    playerOne();
                     break;
             }
         }
-        if(counter+calibrate == wordSize)
+        
+        //If a person fills up blanks, while still having a life, display win screen
+        else if(flag2 == 0)
         {
             system("cls");
-            printf(" LIVES: %d\n\n", persisLives);
+            
+            //Display Lives
+            printf(" LIVES: %d\n", persisLives);
+            printf("\n");
+        
+            //Display blanks
             for(i = 0; i < wordSize; i++)
             {
-                 printf(" %c", blanks[i]);
+                printf(" %c", blanks[i]);
             }
-            //guesses tracker
+    
+            //Display Guesses
             printf("\n\nWRONG GUESSES:\n");
             for(i = 0; i < 9; i++)
             {
                 printf(" %c", wrongChars[i]);
             }
             
+            //Display menu to play again or return to main menu
             printf("\n\nYOU WIN!");
-
             printf("\n\nTYPE '1' TO PLAY AGAIN\nTYPE '2' TO RETURN TO MAIN MENU\n");
             scanf(" %d", &input);
             switch(input)
@@ -215,73 +329,118 @@ int playerOne(void)
                     main();
                     break;
                 default:
-                    printf("PICK '1' OR '2'");
+                    printf("YOU CAN ONLY PICK '1' OR '2'");
+                    playerOne();
                     break;
             }
         }
     }
 }
 
+//Mutliplayer game
 int playerTwo(void)
 {
     system("cls");
-    int wordSize, i, j=0, input, counter = 0;
-    char wordTwo[30], blanks[30], guess, wrongChars[9];
+    int wordSize, i, j=0, k, input, input2, counter = 0;
+    char blanks[30], guess, wrongChars[9];
     int persisLives = counterLives;
+    
+    //Has one user input a word
     printf("\nPLAYER ONE: TYPE ONE WORD (MAKE SURE CAPS IS ON)\n\nDONT LET THE OTHERS SEE IT!\n");
     scanf("%s", &wordTwo);
     wordSize = strlen(wordTwo);
     
-    //Initializing Arrays
+    //Initializing array of blanks
     for(i = 0; i < wordSize; i++)
     {
         blanks[i] = '_';
     }
+    
+    //Initializing array of wrong guesses
     for(i = 0; i < 9; i++)
     {
         wrongChars[i] = '\0';
     }
     
-    //MAIN INTERFACE FOR PLAYER2 GAME
+    //Main interface for game 
     while(persisLives > 0)
     {
-        int flag = 0;
         system("cls");
-        //lives updater
-        printf(" LIVES: %d", persisLives);
-        printf("\n\n");
-        //blanks updater
+        int flag = 0, flag2 = 0;
+        
+        //Display Lives
+        printf(" LIVES: %d\n", persisLives);
+        printf("\n");
+        
+        //Display blanks
         for(i = 0; i < wordSize; i++)
         {
             printf(" %c", blanks[i]);
         }
-        //guesses tracker
+        
+        //Display wrong guesses
         printf("\n\nWRONG GUESSES:\n");
         for(i = 0; i < 9; i++)
         {
             printf(" %c", wrongChars[i]);
         }
-        //input letter, check to see if input character matches word, if not, take away life
+        
+        //Scans for user input and checks to see if input matches a character in generated word
         printf("\n\nGUESS A LETTER\n");
         scanf(" %c", &guess);
+        
+        //Check for duplicates
         for(i=0; i<wordSize; i++)
         {
-            if(wordTwo[i] == guess)
-            {
+            if(wordTwo[i] == guess  && blanks[i] == '_')
+             {
                 printf("\a");
                 blanks[i] = guess;
                 counter++;
                 flag = 1;
-            }
+             }
         }
+        
+        //If the input is not found, remove a life and add to array of wrong guesses
         if(flag == 0)
         {
             persisLives--;
             wrongChars[j] = guess;
-            j++; 
+            j++;
         }
-        if(persisLives == 0)
+        
+        //Checks to find underscores, if there is then change flag
+        for(i = 0; i<wordSize; i++)
         {
+            if(blanks[i] == '_')
+            {
+                flag2 = 1;
+                break;  
+            }
+        }
+        
+        //If the lives reaches zero and flag from above is triggered, display lose screen
+        if(flag2 == 1 && persisLives == 0)
+        {
+            system("cls");
+            
+            //Display final Lives
+            printf(" LIVES: %d\n", persisLives);
+            printf("\n");
+        
+            //Display final blanks
+            for(i = 0; i < wordSize; i++)
+            {
+                printf(" %c", blanks[i]);
+            }
+            
+            //Display final guesses
+            printf("\n\nWRONG GUESSES:\n");
+            for(i = 0; i < 9; i++)
+            {
+                printf(" %c", wrongChars[i]);
+            }
+            
             printf("\n\n\nYOU LOSE, THE CORRECT ANSWER IS %s\n\n\nTYPE '1' TO PLAY AGAIN\nTYPE '2' TO RETURN TO MAIN MENU\n", wordTwo);
             scanf(" %d", &input);
             switch(input)
@@ -293,27 +452,36 @@ int playerTwo(void)
                     main();
                     break;
                 default:
-                    printf("YOU CAN ONLY PICK '1' OR '2'");
+                    printf("PICK '1' OR '2'");
+                    playerTwo();
                     break;
             }
         }
-        if(counter == wordSize)
+        
+        //If a person fills up blanks, while still having a life, display win screen
+        else if(flag2 == 0)
         {
             system("cls");
-            printf(" LIVES: %d\n\n", persisLives);
+            
+            //Display Lives
+            printf(" LIVES: %d\n", persisLives);
+            printf("\n");
+        
+            //Display blanks
             for(i = 0; i < wordSize; i++)
             {
-                 printf(" %c", blanks[i]);
+                printf(" %c", blanks[i]);
             }
-            //guesses tracker
+    
+            //Display Guesses
             printf("\n\nWRONG GUESSES:\n");
             for(i = 0; i < 9; i++)
             {
                 printf(" %c", wrongChars[i]);
             }
             
+            //Display menu to play again or return to main menu
             printf("\n\nYOU WIN!");
-
             printf("\n\nTYPE '1' TO PLAY AGAIN\nTYPE '2' TO RETURN TO MAIN MENU\n");
             scanf(" %d", &input);
             switch(input)
@@ -326,16 +494,18 @@ int playerTwo(void)
                     break;
                 default:
                     printf("YOU CAN ONLY PICK '1' OR '2'");
+                    playerTwo();
                     break;
             }
         }
     }
 }
 
+//Options menu
 int options(void)
 {
-    int inputDiff;
     system("cls");
+    int inputDiff;
     printf("DIFFICULTY OPTIONS:\n");
     printf("PRESS '1' FOR EASY DIFFICULTY\n");
     printf("PRESS '2' FOR MEDIUM DIFFICULTY\n");
@@ -343,6 +513,8 @@ int options(void)
     printf("PRESS '4' TO RETURN TO THE MAIN MENU\n");
     printf("\n\n\n\nCURRENT DIFFICULTY SETTING: %s\n", difficulty);
     scanf("%d", &inputDiff);
+    
+    //Change value of counterLives to selected value
     switch(inputDiff)
     {
         case 1:
@@ -370,6 +542,7 @@ int options(void)
     }
 }
 
+//Help menu
 int help(void)
 {
     char inputChar;
@@ -383,11 +556,13 @@ int help(void)
     }
 }
 
+//Quit program
 int quit(void)
 {
     return 0;
 }
 
+//Functions for wordlists
 int countries(void)
 {
     file = fopen("listCountries.txt", "r");
@@ -400,3 +575,25 @@ int books(void)
 {
     file = fopen("listBooks.txt", "r");
 }
+int winter(void)
+{
+    file = fopen("listWinter.txt", "r");
+}
+int animals(void)
+{
+    file = fopen("listAnimals.txt", "r");
+}
+int people(void)
+{
+    file = fopen("listPeople.txt", "r");
+}
+int brands(void)
+{
+    file = fopen("listBrands.txt", "r");
+}
+int hackathon(void)
+{
+    file = fopen("listHackathon.txt", "r");
+}
+
+//Kade Harmon
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Hangman
+Hangman game constrcuted using the C programming language.
+
+Complete with
+•menus
+•difficult settings
+•6+ categories
+•single player and multiplayer support
+
+
+    0000000000000
+    0           0
+    0           0
+    0          000
+    0           0
+    0          000
+    0         0 0 0
+    0        0  0 0
+    0          0 0
+    0         0   0
+    0        0     0
+    0       0       0
+    0
+    0
+    0
+    0   0   0   0   0   0 
+    
